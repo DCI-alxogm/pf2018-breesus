8e058ce98771f4304d6d22bc4346c2749d87a9d5
Gonzalez Valadez Ulises Aldair <gonzalezvu2018@licifug.ugto.mx>
2018-12-05 09:08:20 -0600

    Comente el archivo de números aleaorios.

--- Archivos_de_prueba/NA.c
+++ Archivos_de_prueba/NA.c
@@ -7,32 +7,34 @@ El objetivo es generar puntos aleatorios. Este archivo sera agregado al codigo f
 #include<time.h>
 
 int main(){
-	FILE*Ale;
-        float x, y, z, n;
-        int i;
-        srand(time(NULL));
-        srand48(time(NULL));  
+	FILE*Ale;          //Se declara una variable para abrir el archivo donde se guardarán los números.
+        float x, y, z, n;  //Se declaran los flotantes para los vectores unitarios de posición y controlar los números aleatorios.
+        int i;             //Se declara una variable para controlar funciones.
+        srand(time(NULL));    //Se introduce una "semilla" para los números aleatorios enteros con relación a la hora de compilación.
+        srand48(time(NULL));  //Se introduce una "semilla" para los números aleatorios flotantes con relación a la hora de compilación.
         
-        printf("Presione 1 para obtener un número aleatorio de puntos aleatorios. \n Presione 2 para obtener un número especifico de puntos aleatorios.");
-        scanf("%i", &i);
+        printf("Presione 1 para obtener un número aleatorio de puntos aleatorios. \n Presione 2 para obtener un número especifico de puntos aleatorios.");  //Se da la opción de escoger la cantidad de púntos, o ser también un número aleatorio.
+
+        scanf("%i", &i); 
+
         switch(i){
-               case 1:
+               case 1:                         //En caso de ser un número aleatorio, el programa escogera un número al azar y lo usara despues para el cilo for.
                       n=rand() % 11;
                       printf("El número de puntos será %f", n);
                       break;
-               case 2:
-                      printf("Introduzca el número de puntos a generar.\n");
+               case 2:      //En caso de ser un número de puntos determinado, se pedira dicho número.
+                      printf("Introduzca el número de puntos a generar.\n"); 
                       scanf("%f", &n);
                       printf("El número de puntos sera %f", n);
                       break;
                }
          Ale=fopen("Aleatorios.txt", "w");
-         fprintf(Ale, "x\t\t y\t\t z \n");
+         fprintf(Ale, "x\t\t y\t\t z \n");         //Se genera el archivo y se prepara para escribir los puntos aleatorios.
          for(i=0; i<n; i++){
             x=drand48() * (20.0 - 0.0) + 0.0;
-            y=drand48() * (20.0 - 0.0) + 0.0;
+            y=drand48() * (20.0 - 0.0) + 0.0;   //Se escriben números aleatorios por sus componentes, usando un rango de 0 a 20, cosa que despues sera modificada con el tamaño del cubo.
             z=drand48() * (20.0 - 0.0) + 0.0;
-         fprintf(Ale, "%f\t %f\t %f \n", x, y, z);
+         fprintf(Ale, "%f\t %f\t %f \n", x, y, z);  //Se imprimen los puntos en el archivo generado.
          }
      return 0;
 }
