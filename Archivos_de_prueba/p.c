f60a56f04e11578306564df1ea6c68b2d30cc40d
Aldair Gonzalez <aldairgonzalez@localhost.localdomain>
2018-12-06 17:13:08 -0600

    Avance 6/12/18.

--- Archivos_de_prueba/p.c
+++ Archivos_de_prueba/p.c
@@ -2,18 +2,35 @@
 #include<math.h>
 #include<stdio.h>
 #include<time.h>
+#include <string.h>
 
 void Aleatorios(int n);
-int Lineas();
+int Lineas(char Nombre[40]);
 
 int main(){
-    int n;
-    int l;
+    int n, l[10], i;
+    char Respuesta[3], Nombre[40];
     
     printf("Introduzca el número de puntos aleatorios a generar.\n");
     scanf("%i", &n);
     Aleatorios(n);
-    l=Lineas();
-    printf("El archivo tiene %i lineas.\n", l);
+    printf("¿De qué archivo se tomarán los puntos?");
+    l[0]=Lineas(Nombre);
+    printf("El archivo tiene %i lineas. \n", l[0]);
+    printf("¿Hay otro archivo del que tomar los puntos?");
+    scanf("%s", Respuesta);
+    if(strcmp(Respuesta, "Si")==0){
+         printf("Escriba bien, puerco.\n");
+         }
+    for(i=1; strcmp(Respuesta, "Sí") == 0; i++){
+      l[i]=Lineas(Nombre);
+      printf("El archivo tiene %i lineas.\n", l[i]);
+      printf("¿Hay otro archivo del que tomar los puntos?");
+      scanf("%s", Respuesta); 
+      if(strcmp(Respuesta, "Si")==0){
+         printf("Escriba bien, puerco");
+         }     
+      }
+    
     return 0;
 }
