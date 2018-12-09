a71cf96a9b6bc82d048fc2a0a036f0eaa2234833
Juan Paz <juanpaz@localhost.localdomain>
2018-12-09 11:58:22 -0600

    Avance

--- Archivos_de_prueba/p.c
+++ Archivos_de_prueba/p.c
@@ -18,7 +18,7 @@ int main(){
     scanf("%s", Nombre1);
     l1=Lineas(Nombre1);
     printf("El archivo tiene %i lineas. \n", l1);
-    printf("¿Quíere usar un archivo con puntos aleatorios para los estimadores?(S/N)");
+    printf("¿Quíere usar un archivo con puntos aleatorios para los estimadores?(S/N)\n");
     scanf("%s", Respuesta);
     if(strcmp(Respuesta, "S") == 0){
       printf("Introduzca el número de puntos aleatorios a generar.\n");
@@ -26,7 +26,7 @@ int main(){
       Aleatorios(l2);
       
       }else{
-      printf("¿De que otro archivo se tomarán los puntos?");
+      printf("¿De que otro archivo se tomarán los puntos?\n");
       scanf("%s", Nombre2);
       l2=Lineas(Nombre2);
       printf("El archivo tiene %i lineas.\n", l2);
@@ -41,6 +41,7 @@ int main(){
     Arc=fopen(Nombre1, "r");
     for(i=0; i<l1; i++){
        fscanf(Arc, "%f %f %f", &x[i], &y[i], &z[i]);
+      
 }
 fclose(Arc);
 printf("valores de r para el archivo %s\n", Nombre1);
@@ -50,22 +51,40 @@ printf("valores de r para el archivo %s\n", Nombre1);
            printf("%f\n", s);
           }}
 
-Arc2=fopen(Nombre2, "r");
+Arc=fopen(Nombre1, "r");
     for(i=0; i<l1; i++){
        fscanf(Arc, "%f %f %f", &x[i], &y[i], &z[i]);
+      
+}
+fclose(Arc);
+Arc2=fopen(Nombre2, "r");
+    for(i=0; i<l2; i++){
+       fscanf(Arc2, "%f %f %f", &x[j], &y[j], &z[j]);
+       
+}
+fclose(Arc2);
+printf("valores de r para los archivos %s y %s\n", Nombre1, Nombre2);
+ for(i=0; i<l2; i++){
+       for(j=0;j<l2;j++){
+           s=sqrt((pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)+pow((z[i]-z[j]),2)));
+           printf("%f\n", s);
+          }}
+Arc2=fopen(Nombre2, "r");
+    for(i=0; i<l2; i++){
+       fscanf(Arc2, "%f %f %f", &x[i], &y[i], &z[i]);
+       
 }
 fclose(Arc2);
 printf("valores de r para el archivo %s\n", Nombre2);
-     for(i=0; i<l1; i++){
-       for(j=(i+1);j<l1;j++){
+     for(i=0; i<l2; i++){
+       for(j=(i+1);j<l2;j++){
            s=sqrt((pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)+pow((z[i]-z[j]),2)));
            printf("%f\n", s);
           }}
 
+ 
 return 0;
 }
-
-
 
     
     
